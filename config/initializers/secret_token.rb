55baf58adac7c4c8b750381e3820e7aaacee2dc6
Paul Moore <pmmoor1@gmail.com>
2012-07-05 16:31:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BcwRails::Application.config.secret_token = 'c955fde306e13e3e50469ed2af909d7fff9967a10664297a4b1db9777f7ff0552a912ad5add060749981420ec1a793b4c411a9f1ba647c18ddd6e0206e5b9863'
